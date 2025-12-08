b519066bd59416d69dcbfcb38d3d8ec3894dea6d
Volker Janz <volker@janz.sh>
2025-12-07 19:24:50 -0800

    Unify link order

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const config = {
     ],
 
     links: [
-        { label: "GitHub",    url: "https://github.com/vojay-dev",       icon: "fab fa-github" },
         { label: "LinkedIn",  url: "https://linkedin.com/in/vjanz",      icon: "fab fa-linkedin" },
+        { label: "GitHub",    url: "https://github.com/vojay-dev",       icon: "fab fa-github" },
         { label: "X",    url: "https://twitter.com/vojaydev",            icon: "fab fa-x-twitter" },
         { label: "Instagram", url: "https://instagram.com/vojay.io",     icon: "fab fa-instagram" }
     ]
