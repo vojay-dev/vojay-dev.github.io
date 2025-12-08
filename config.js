c22299808b3d949fed08425f8bd95316f7431605
Volker Janz <volker@janz.sh>
2025-12-07 18:16:52 -0800

    Update config

--- config.js
+++ config.js
@@ -1,23 +1,16 @@
-// User Configuration
 const config = {
-    // Title of the browser tab
     title: "root@vjanz:~",
-
-    // The markdown file to open on load (without .md)
     startPage: "home",
 
-    // FILES: These will appear in the Sidebar
-    // The engine looks for them in the 'content/' folder
     files: [
         "home",
         "about"
     ],
 
-    // LINKS: External websites
     links: [
         { label: "GitHub",    url: "https://github.com/vojay-dev",       icon: "fab fa-github" },
         { label: "LinkedIn",  url: "https://linkedin.com/in/vjanz",      icon: "fab fa-linkedin" },
-        { label: "X / Tw",    url: "https://twitter.com/vojaydev",       icon: "fab fa-x-twitter" },
+        { label: "X",    url: "https://twitter.com/vojaydev",            icon: "fab fa-x-twitter" },
         { label: "Instagram", url: "https://instagram.com/vojay.io",     icon: "fab fa-instagram" }
     ]
 };
